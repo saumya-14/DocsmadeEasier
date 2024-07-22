1de6ef0c5ff339f27c842504c99478e1ca2e3d0b
saumya-14 <saumya140904@gmail.com>
2024-07-22 13:04:25 +0530

    first

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import React from 'react'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+const Document = () => {
+  return (
+    <div>
+      <Header>
+       <div className='flex w-fit items-center justify-center gap-2'>
+        <p className='document-title'>This is a fake document </p>
+
+       </div>
+       <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
+      <Editor/>
+    </div>
+  )
+}
+
+export default Document
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+   <Button>Click Me</Button>
+  )
+}
+
+export default Home
